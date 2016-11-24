9622031778b844acbe0467e9ab979e01ee1d57f6
Lucas Ferreira <lucas.182.ferreira@hotmail.com>
2016-11-24 00:02:58 -0300

    Adições de camada de negócios + atualização repositórios

--- MedicineCenter/src/negocio/NGProcedimento.java
+++ MedicineCenter/src/negocio/NGProcedimento.java
@@ -4,16 +4,61 @@ import classes_basicas.Procedimento;
 import dao.IRepositorioProcedimento;
 import dao.RepositorioProcedimento;
 import exceptions.DatabaseException;
+import java.util.ArrayList;
 
 
 public class NGProcedimento{
-    private IRepositorioProcedimento repProc = new RepositorioProcedimento();
-
-    public void inserir(Procedimento proc) throws DatabaseException {
+    IRepositorioProcedimento repProcedimento = new RepositorioProcedimento();
+    
+    public void inserirProcedimento(Procedimento p) throws DatabaseException{
         try{
-            repProc.inserir(proc);
-        }catch(DatabaseException ex){
-            throw new DatabaseException(ex);
+            repProcedimento.inserir(p);
+        }catch(DatabaseException e){
+           throw new DatabaseException(e);
+        }
+    }
+    
+    public void excluirProcedimento(Procedimento p) throws DatabaseException{
+        try{
+            repProcedimento.excluir(p);
+        }catch(DatabaseException e){
+           throw new DatabaseException(e);
+        }
+    }
+    
+    public void alterarProcedimento(Procedimento p) throws DatabaseException{
+        try{
+            repProcedimento.alterar(p);
+        }catch(DatabaseException e){
+           throw new DatabaseException(e);
+        }
+    }
+    
+    public ArrayList<Procedimento> listarProcedimento(Procedimento p) throws DatabaseException{
+        try{
+            return repProcedimento.listar();
+        }catch(DatabaseException e){
+           throw new DatabaseException(e);
+        }
+    }
+    
+    public ArrayList<Procedimento> pesquisarProcedimento(int cod_procedimento) throws DatabaseException{
+        try{
+            Procedimento proc = new Procedimento();
+            proc.setCod_procedimento(cod_procedimento);
+            return repProcedimento.pesquisar(proc);
+        }catch(DatabaseException e){
+            throw new DatabaseException(e);
+        }
+    }
+    
+    public ArrayList<Procedimento> pesquisarProcedimentoPorDescricao(String descricao) throws DatabaseException{
+        try{
+            Procedimento proc = new Procedimento();
+            proc.setDescricao(descricao);
+            return repProcedimento.pesquisarProcedimentoPorDescricao(proc.getDescricao());
+        }catch(DatabaseException e){
+            throw new DatabaseException(e);
         }
     }
 
